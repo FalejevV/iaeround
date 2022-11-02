083ea8d8a06e97b1fb24e3b2385d8b33910870ed
FalejevV <v.o.v.a@hotmail.com>
2022-11-02 20:38:38 +0200

    removed backend folder

--- backend/database/routes/Tag.routes.js
+++
@@ -1,8 +0,0 @@
-import Router from 'express';
-const router = new Router();
-import TagController from '../controllers/Tag.controller.js';
-
-router.get("/tag", TagController.getTags);
-
-
-export default router;
